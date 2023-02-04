e6d1d563d7b3c2d5e9829fbabeca2f624236451c
Aditya Gaddhyan <adityagaddhyan@gmail.com>
2023-02-04 23:37:09 +0530

    Error boundaru

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  constructor(props){
+    super(props)
+    this.state = { hasError: false };
+  }
+  
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log("Component did catch caught error", error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an Error.{" "}
+          <Link to="/">Click here to go back to home page</Link>
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
--- src/Results.jsx
+++ src/Results.jsx
@@ -4,11 +4,24 @@ const Results = ({ pets }) => {
   return (
     <div className="search">
       {!pets.length ? (
-        <h1>No Pets found!</h1>
+        <h1>No Pets Found</h1>
       ) : (
-        pets.map((p) => <Pet {...p} key={p.id} />)
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
       )}
     </div>
   );
 };
+
 export default Results;
